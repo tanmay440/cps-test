6c09f348f7db474d78cf4248f1385efe93c54c87
tanmay440 <62497552+tanmay440@users.noreply.github.com>
2021-11-01 10:17:14 +0530

    got a really bad cps
    :(

--- buttons.py
+++ buttons.py
@@ -1,3 +1,6 @@
+import pygame
+pygame.init()
+pygame.font.init()
 def get_font(size):
     return pygame.font.SysFont("comicsans", size)
 
@@ -13,10 +16,8 @@ class Button:
         self.text_color = text_color
 
     def draw(self, win):
-        pygame.draw.rect(
-            win, self.color, (self.x, self.y, self.width, self.height))
-        pygame.draw.rect(
-            win, BLACK, (self.x, self.y, self.width, self.height), 2)
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(win, (0, 0, 0), (self.x, self.y, self.width, self.height), 10)
         if self.text:
             button_font = get_font(22)
             text_surface = button_font.render(self.text, 1, self.text_color)
--- pgaaaaaaaaaaaaame.py
+++ pgaaaaaaaaaaaaame.py
@@ -3,9 +3,22 @@ import pygame
 win = pygame.display.set_mode((700, 700))
 run = True
 clock = pygame.time.Clock()
+main = buttons.Button(100, 100, 500, 500, (255, 255, 255), "click me")
+x = 0
 while run:
     clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    if pygame.mouse.get_pressed()[0]:
+            mouseX, mouseY = pygame.mouse.get_pos()
+            if main.clicked((mouseX, mouseY)):
+                x+=1
+    win.fill((107, 238, 184))
+    main.draw(win)
+    pygame.display.flip()
+    cps = x/60
+    with open("cps.txt", "w") as file:
+        file.write(f"{round(cps, 2)} cps")
+    print(cps)
 pygame.quit()
