298885605565a3ce748d0447e4aa6c8d0d17e6b4
tanmay440 <62497552+tanmay440@users.noreply.github.com>
2021-11-01 09:46:49 +0530

    Bacic

--- pgaaaaaaaaaaaaame.py
+++ pgaaaaaaaaaaaaame.py
@@ -1,2 +1,10 @@
-from pygame import *
-win = display.set_mode((700, 700))
+import pygame
+win = pygame.display.set_mode((700, 700))
+run = True
+
+while run:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+pygame.quit()
